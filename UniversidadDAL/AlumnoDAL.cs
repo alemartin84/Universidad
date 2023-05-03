6e59d227fbe27cad7bd8e8a3ea2fc95e262d40af
Alejandro Martin <alejandroemilianomartin@hotmail.com>
2023-05-03 20:38:44 -0300

    agrego sqlite

--- UniversidadDAL/AlumnoDAL.cs
+++ UniversidadDAL/AlumnoDAL.cs
@@ -1,4 +1,7 @@
-﻿using UniversidadEntidades;
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using UniversidadEntidades;
+
 
 namespace UniversidadDAL
 {
@@ -6,7 +9,18 @@ namespace UniversidadDAL
     {
         public Alumno GetAlumno(int id)
         {
-            return new Alumno();
+            using (var connection = new SqliteConnection(@"Data Source=f:\DBcarrera.db"))
+            {
+                //connection.Open();
+
+                var sql =
+                @"SELECT *
+                    FROM alumno";
+
+                var alumno = connection.QueryFirst<Alumno>(sql);
+
+                return alumno;
+            }
         }
     }
 
--- UniversidadEntidades/Alumno.cs
+++ UniversidadEntidades/Alumno.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public string Apellido { get; set; } = string.Empty;
-        public DateTime FechaNacimiento { get; set; } = DateTime.Today;
-        public DateTime FechaAlta { get; set; } = DateTime.Today;
+        public DateTime Fecha_Nacimiento { get; set; } = DateTime.Today;
+        public DateTime Fecha_Alta { get; set; } = DateTime.Today;
     }
 }
